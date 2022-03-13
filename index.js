7d85a651257e2e64905e1b6d7f2ed99efa2a35d0
YoshiBNE <yoshiyasu_express@me.com>
2022-03-14 00:27:09 +1000

    D one

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
 // Code your solutions in this file
+
+function writeCards(arr, adj) {
+    let newArr = [];
+    for (let i=0; i<arr.length; i++) {
+        newArr.push(`Thank you, ${arr[i]}, for the wonderful ${adj} gift!`);
+    }
+    return newArr;
+}
+
+function countDown(num) {
+    let i = num;
+    while (i>-1) {
+        console.log(i);
+        i--;
+    }
+}
